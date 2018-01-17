4305953cba566b7cedffa4794217e4182d718610
Asher <aklein@descriptsw.com>
2018-01-17 11:30:13 -0500

    Add wether_list

---
+++ src/containers/weather_list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class WeatherList extends Component {
+    render() {
+        return (
+            <table classNAme='table table-hover'>
+                <thead>
+                    <tr>
+                        <th>CIty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        )
+    }
+}
